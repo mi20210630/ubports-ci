33caecce5041c1daaba56c532b0cf4180391b9cd
ShanghaiZhouWanLi <106662135+mi20210630@users.noreply.github.com>
2023-08-05 20:40:09 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -7,13 +7,7 @@ source build/envsetup.sh
 bash hybris-patches/apply-patches.sh --mb
 export USE_CCACHE=1
 breakfast $DEVICE
-make -j$(nproc) mkbootimg
-make -j$(nproc) fec
-make -j$(nproc) halium-boot
-make -j$(nproc) systemimage 
-make -j$(nproc) vendorimage
-
-echo "md5sum halium-boot.img system.img vendor.img"
-md5sum $ANDROID_ROOT/out/target/product/clover/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/clover/system.img
-md5sum $ANDROID_ROOT/out/target/product/clover/vendor.img
+mka mkbootimg
+export USE_HOST_LEX=yes
+mka hybris-boot
+mka systemimage

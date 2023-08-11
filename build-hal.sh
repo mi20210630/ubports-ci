2dcab9d324eab8d9773ba16a1609d8f12c08db0c
ShanghaiZhouWanLi <106662135+mi20210630@users.noreply.github.com>
2023-08-11 14:31:10 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -6,11 +6,12 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 bash hybris-patches/apply-patches.sh --mb
 export USE_CCACHE=1
-breakfast $DEVICE
+#breakfast $DEVICE
 #grep "TARGET_KERNEL_CONFIG" device/<VENDOR>/<CODENAME>/BoardConfig.mk
 ./halium/halium-boot/check-kernel-config device/xiaomi/cereus/BoardConfig.mk -w
 ./halium/halium-boot/check-kernel-config device/xiaomi/cereus/BoardConfig.mk -w
 ./halium/halium-boot/check-kernel-config device/xiaomi/cereus/BoardConfig.mk
+breakfast $DEVICE
 mka mkbootimg
 export USE_HOST_LEX=yes
 mka halium-boot

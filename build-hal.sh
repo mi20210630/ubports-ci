785b1c0e4cdb75cf1b5a17608646f0d632155a82
ShanghaiZhouWanLi <106662135+mi20210630@users.noreply.github.com>
2023-08-14 10:03:40 +0800

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 source halium.env
 cd $ANDROID_ROOT
-
+sudo apt install git gnupg flex bison gperf build-essential \
+zip bzr curl libc6-dev libncurses5-dev:i386 x11proto-core-dev \
+libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-glx:i386 \
+libgl1-mesa-dev g++-multilib mingw-w64-i686-dev tofrodos \
+python3-markdown libxml2-utils xsltproc zlib1g-dev:i386 schedtool \
+liblz4-tool bc lzop imagemagick libncurses5 rsync \
+python-is-python3 python2
 
 source build/envsetup.sh
 bash hybris-patches/apply-patches.sh --mb
